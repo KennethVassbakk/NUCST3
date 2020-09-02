e8c9d449408a21083e2a0c806ecbe852af4bad09
MrSpectacle <kenneth.vassbakk@gmail.com>
2020-09-02 13:02:51 +0200

    Addede Locomotion animation

---
+++ Assets/_Project/Scripts/Character/CharacterAnimation.cs
@@ -0,0 +1,78 @@
+﻿// Author: Kenneth Vassbakk
+
+using UnityEngine;
+
+namespace Character
+{
+    public class CharacterAnimation : MonoBehaviour
+    {
+        private Animator _animator;
+        
+        // Animator Settings
+        private int _aMovementSpeed;
+        private int _aHorizontal;
+        private int _aVertical;
+        
+        // Store current values
+        private float _aCurrentMovementSpeed;
+        private float _aCurrentHorizontal;
+        private float _aCurrentVertical;
+
+        private void Start()
+        {
+            _animator = GetComponent<Animator>();
+            
+            // Hash animator values to optimize performance
+            _aMovementSpeed = Animator.StringToHash("MovementSpeed");
+            _aHorizontal = Animator.StringToHash("Horizontal");
+            _aVertical = Animator.StringToHash("Vertical");
+            
+        }
+        
+        /// <summary>
+        /// Animate character based on its rigidbody and the movement vector.
+        /// Rotation of the character is taken into accounnt.
+        /// </summary>
+        /// <param name="rb">Character Rigidbody</param>
+        /// <param name="moveVector">Vector3 MovementDirection (input)</param>
+        public void AnimateMovement(Rigidbody rb, Vector3 moveVector)
+        {
+            _aCurrentHorizontal = _animator.GetFloat(_aHorizontal);
+            _aCurrentVertical = _animator.GetFloat(_aVertical);
+
+            var moveVectorNormalized = moveVector.normalized;
+            
+            // If we're not really moving, we dont need to run much
+            // Just make sure we're setting movementspeed to zero and
+            // resetting the animation floats - to make it smoother.
+            if (moveVector.magnitude < 0.1f)
+            {
+                _animator.SetFloat(_aMovementSpeed, 0f);
+                
+                // We also reset the horizontal and vertical of the animator; lerping to smooth transition
+                _animator.SetFloat(_aHorizontal, (_aCurrentHorizontal > 0.05f) ? Mathf.Lerp(_aCurrentHorizontal, 0f, 0.25f) : 0f);
+                _animator.SetFloat(_aVertical, (_aCurrentVertical > 0.05f) ? Mathf.Lerp(_aCurrentVertical, 0f, 0.25f) : 0f);
+                return;
+            }
+            
+            var position = rb.position;
+            
+            // Point we're moving towards relative to the player location
+            moveVector = new Vector3(moveVector.x, position.y, moveVector.z) + position;
+            var moveToward = moveVector - position;
+
+            moveToward = moveToward.normalized;
+            
+            var relativePosition = rb.transform.InverseTransformDirection(moveToward);
+            
+            // We currently dont need to know the relative Angle, but here's the method for it.
+            // var relativeAngle = Vector3.Angle(_rb.transform.forward, moveToward);
+            
+            // Set Animation Settings
+            _animator.SetFloat(_aMovementSpeed, moveVectorNormalized.magnitude);
+            _animator.SetFloat(_aHorizontal, Mathf.Lerp(_aCurrentHorizontal, relativePosition.x, 0.25f));
+            _animator.SetFloat(_aVertical, Mathf.Lerp(_aCurrentVertical, relativePosition.z, 0.25f));
+
+        }
+    }
+}
--- Assets/_Project/Scripts/Character/CharacterMotor.cs
+++ Assets/_Project/Scripts/Character/CharacterMotor.cs
@@ -7,32 +7,41 @@ namespace Character
     public class CharacterMotor : MonoBehaviour
     {
         private CharacterInputController _input;
-
-        [SerializeField] private bool rotateTowardsMouse;
+        
         [SerializeField] private float moveSpeed = 5f;
         private Rigidbody _rb;
         private Camera _cam;
         private Plane _intersectPlane;
-    
-        private void Awake()
+        private CharacterAnimation _characterAnimation;
+        
+        private void OnEnable()
         {
             _input = GetComponent<CharacterInputController>();
+            _characterAnimation = GetComponent<CharacterAnimation>();
             _rb = GetComponent<Rigidbody>();
             _cam = Camera.main;
 
+            // Used for mouse rotation
             _intersectPlane = new Plane(Vector3.up, Vector3.zero);
+            
         }
     
         private void FixedUpdate()
         {
             // Character Movement
-            var moveVector = MoveTowardsVector(_input.inputVector);
-        
-            // Look towards mouse, or towards our direction
+            MoveTowardsVector(_input.inputVector);
+            _characterAnimation.AnimateMovement(_rb, _input.inputVector);
+            
+            // Handle character Rotation
+            RotateToMouse(_input.mousePosition);
+            
+            /*Look towards mouse, or towards our direction
             if (rotateTowardsMouse)
                 RotateToMouse(_input.mousePosition);
             else
                 RotateToDirection(moveVector);
+            */
+            
         }
 
         private void RotateToMouse(Vector2 inputMousePosition)
@@ -51,27 +60,37 @@ namespace Character
             _rb.rotation = Quaternion.Euler(0, angle, 0);
         }
     
+        
+        /*
+        // Keeping this in, so we have it in case we need it for something later.
         private void RotateToDirection(Vector3 moveVector)
         {
             // If we're moving slowly, dont bother
             if (moveVector.magnitude < 0.1f)
+            {
                 return;
+            }
 
             var rotation = Quaternion.LookRotation(moveVector);
             _rb.rotation = Quaternion.RotateTowards(_rb.rotation, rotation, moveSpeed * Mathf.PI);
-        }
-
+        }*/
+        
+        // The return is not currently used, but we might use it sometime.
+        // ReSharper disable once UnusedMethodReturnValue.Local
         private Vector3 MoveTowardsVector(Vector3 moveVector)
         {
+            var moveVectorNormalized = moveVector.normalized;
             // If we dont have  any magnitude to our movement, dont run.
             if (moveVector.magnitude < 0.1f)
+            {
                 return Vector3.zero;
-        
-            moveVector = Quaternion.Euler(0f, _cam.transform.eulerAngles.y, 0f) * moveVector.normalized;
+            }
+            moveVector = Quaternion.Euler(0f, _cam.transform.eulerAngles.y, 0f) * moveVectorNormalized;
             _rb.MovePosition(_rb.position + moveVector * (moveSpeed * Time.fixedDeltaTime));
-        
-            // Return our direction
+            
             return moveVector;
         }
+
+
     }
 }
