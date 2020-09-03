a7e5a8c8fcf8aa60a3e61a38d08ba7cdd090cde0
JohnH <51337462+JohnHauge@users.noreply.github.com>
2020-09-03 21:18:36 +0200

    Hub & Scene load w/Transition

---
+++ Assets/_Project/Scripts/Scene/LoadScene.cs
@@ -0,0 +1,36 @@
+﻿// Author: John Hauge
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    private SceneTransition transition;
+    public int SceneNR;
+
+    private void Start()
+    {
+        transition = FindObjectOfType<SceneTransition>();
+    }
+
+    public void LoadTheScene(int SceneInt)
+    {
+        SceneManager.LoadScene(SceneInt);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            transition.DrawCurtains(this);
+        }
+    }
+
+    public void CurtainClosed()
+    {
+        LoadTheScene(SceneNR);
+    }
+    
+}
---
+++ Assets/_Project/Scripts/Scene/SceneTransition.cs
@@ -0,0 +1,64 @@
+﻿// Author: John Hauge
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour
+{
+    private Image image;
+
+    private bool fadein;
+    private LoadScene load;
+
+    private float t;
+    public float delay;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+
+        //
+
+        t = 1f;
+        image.color = new Color(0f, 0f, 0f, 1f);
+        fadein = true;
+        enabled = true;
+    }
+
+    private void FixedUpdate()
+    {
+
+        if (fadein)
+        {
+            t -= Time.deltaTime / delay;
+            if (t < 0)
+            {
+                enabled = false;
+                return;
+            }
+        }
+        else
+        {
+            t += Time.deltaTime / delay;
+            if (t > 1)
+            {
+                enabled = false;
+                load.CurtainClosed();
+                return;
+            }
+        }
+        image.color = new Color(0f, 0f, 0f, t);
+    }
+
+
+    public void DrawCurtains(LoadScene loadScene)
+    {
+        t = 0;
+        image.color = new Color(0f, 0f, 0f, 0f);
+        fadein = false;
+        enabled = true;
+        load = loadScene;
+    }
+}
