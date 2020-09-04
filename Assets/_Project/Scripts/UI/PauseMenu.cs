1b1af5cbb2e55043aa8c223573928e16575f4501
JohnH <51337462+JohnHauge@users.noreply.github.com>
2020-09-04 11:09:42 +0200

    PauseMenu & Placeholder UI

---
+++ Assets/_Project/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿// Author: John Hauge
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private UI ui;
+
+    private void Awake()
+    {
+        ui = GetComponentInParent<UI>();
+    }
+
+    public void toGame()
+    {
+        ui.TogglePause();
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/_Project/Scripts/UI/UI.cs
@@ -0,0 +1,35 @@
+﻿// Author: John Hauge
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public GameObject PauseMenu_GO;
+
+    void Update()
+    {
+       //to be hooked up to the new input system.
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        if (PauseMenu_GO.activeSelf)
+        {
+            PauseMenu_GO.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            PauseMenu_GO.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+}
